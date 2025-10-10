e77336fe3c5f201ab539089f9e4022399a766217
Sandra Kawombe <sandrakawombe@gmail.com>
2025-10-10 11:09:33 -0500

    added @Query in coruse repository

--- src/main/java/snakayima/miu/edu/studentcoursemanagementsystem/Repository/CourseRepository.java
+++ src/main/java/snakayima/miu/edu/studentcoursemanagementsystem/Repository/CourseRepository.java
@@ -1,8 +1,20 @@
 package snakayima.miu.edu.studentcoursemanagementsystem.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import snakayima.miu.edu.studentcoursemanagementsystem.Model.Course;
+
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    //JPQL
+    @Query(value ="select c from Course c where c.courseId=:course_id")
+    List<Course> getAllCourseById(@Param("course_id") Integer id);
+
+    //Native Query
+    List<Course> getAllCoursesByName(String name);
 }
